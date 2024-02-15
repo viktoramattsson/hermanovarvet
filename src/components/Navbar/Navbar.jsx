2db56d7a264e40d4597a13d931936495d7a28228
Viktor Mattsson <viktor.a.mattsson@gmail.com>
2024-02-15 18:46:10 +0100

    remove logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import logo from './../../../public/logo.png';
+//import logo from '../../../public/logo.png';
 import {
   AiOutlineMenu,
   AiOutlineClose,
@@ -26,9 +26,9 @@ const Navbar = () => {
     <>
       <nav className="fixed w-full h-24 shadow-xl bg-white rounded-b-lg z-10">
         <div className="flex justify-between items-center h-full w-full px-8 2xl: px16">
-          <Link href="/">
+          {/* <Link href="/">
             <Image src={logo} alt="logo" width={120} height={120} />
-          </Link>
+          </Link> */}
           <div className="hidden lg:flex">
             <ul className="hidden lg:flex">
               <Link href="/register">
@@ -196,9 +196,9 @@ const Navbar = () => {
             </div>
 
             <div>
-              <Link href="/">
+              {/* <Link href="/">
                 <Image src={logo} alt="logo" width={100} height={100} />
-              </Link>
+              </Link> */}
             </div>
           </div>
         </div>
